0157c46090ba05385e70ab0426917690f35f5a2d
Camilo Rodriguez <crodriguez@auronix.com>
2024-10-21 16:07:22 +0000

    Added repository

--- bin/code-generator/repository.js
+++ bin/code-generator/repository.js
@@ -1,5 +1,5 @@
 import { promises } from "fs";
-import { kebabCase, remplazeInFile, pascalCase, processLineByLine } from './utils.js';
+import { kebabCase, remplazeInFile, pascalCase, processLineByLine, camelCase } from './utils.js';
 
 export default class Repository {
   stubFolder = process.cwd() + '/node_modules/@sdkconsultoria/nestjs-base/bin/code-generator/stubs/';
@@ -10,7 +10,7 @@ export default class Repository {
     this.validate(args);
     await this.writteSchema(args);
     await this.writteRepository(args);
-
+    await this.writteFactory(args);
   }
 
   validate(args) {
@@ -37,6 +37,19 @@ export default class Repository {
     await promises.cp(`${this.stubFolder}usecases/model.repository.ts.stub`, `${this.outFolderRepository}${kebabCase(args[4])}.repository.ts`);
     await remplazeInFile(`${this.outFolderRepository}${kebabCase(args[4])}.repository.ts`, '{{modelClass}}', pascalCase(args[4]));
     await remplazeInFile(`${this.outFolderRepository}${kebabCase(args[4])}.repository.ts`, '{{modelFile}}', kebabCase(args[4]));
+
+    await promises.cp(`${this.stubFolder}usecases/model.repository.spec.ts.stub`, `${this.outFolderRepository}${kebabCase(args[4])}.repository.spec.ts`);
+    await remplazeInFile(`${this.outFolderRepository}${kebabCase(args[4])}.repository.spec.ts`, '{{modelFile}}', kebabCase(args[4]));
+    await remplazeInFile(`${this.outFolderRepository}${kebabCase(args[4])}.repository.spec.ts`, '{{modelClass}}', pascalCase(args[4]));
+    await remplazeInFile(`${this.outFolderRepository}${kebabCase(args[4])}.repository.spec.ts`, '{{modelCamelCase}}', camelCase(args[4]));
+
+  }
+
+  async writteFactory(args) {
+    const propierties = await this.loadPropertiesFactory(args);
+    await promises.cp(`${this.stubFolder}model.factory.ts.stub`, `${process.cwd()}/test/factory/${kebabCase(args[4])}.factory.ts`);
+    await remplazeInFile(`${process.cwd()}/test/factory/${kebabCase(args[4])}.factory.ts`, '{{modelClass}}', pascalCase(args[4]));
+    await remplazeInFile(`${process.cwd()}/test/factory/${kebabCase(args[4])}.factory.ts`, '{}', `{\n${propierties.join('\n')}\n  }`);
   }
 
   async loadProperties(args) {
@@ -45,6 +58,32 @@ export default class Repository {
     for (let index = 0; index < data.length; index++) {
       if (data[index].includes(':')) {
         props.push('\n  @Prop({ required: true })\n'+data[index]);
+      }
+    }
+
+    props.push('\n  @Prop({ index: true })\n  deletedAt: Date;');
+
+    return props;
+  }
+
+  async loadPropertiesFactory(args) {
+    const data = await processLineByLine(`${process.cwd()}/src/entities/${kebabCase(args[4])}.model.ts`);
+    const props = [];
+    for (let index = 0; index < data.length; index++) {
+      if (data[index].includes(':')) {
+        const type = data[index].split(':');
+
+        switch (type[1].trim()) {
+          case 'string;':
+            props.push(`  ${type[0]}: faker.word.adjective(),`);
+            break;
+          case 'number;':
+            props.push(`  ${type[0]}: faker.number.bigInt(),`);
+          break;
+          default:
+            props.push(`  ${type[0]}: null,`);
+            break;
+        }
       }
     }
 
