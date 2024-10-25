baa48f38039183689a92c18e13ee60bbbfe9c3c5
Camilo Rodriguez <crodriguez@auronix.com>
2024-10-25 22:11:05 +0000

    Update health

--- bin/scripts/init-project/healthcheck.js
+++ bin/scripts/init-project/healthcheck.js
@@ -5,6 +5,7 @@ export default class Health extends BaseScript {
         if (this.settings.healthPage) {
             console.log('Instalando dependencia @nestjs/terminus...');
             await this.execute('npm install --save @nestjs/terminus');
+            await this.execute('npm link  @sdkconsultoria/nestjs-base');//////////////-*****************************
             await this.copyFolderFromArchitectureFolder('interface-adapters/controllers/health/', 'entity');
             this.addModuleToControllerModule();
         }
