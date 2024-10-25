8e9c60d74e18e8e115c30fcded60e360e36c7e87
Camilo Rodriguez <crodriguez@auronix.com>
2024-10-25 17:30:52 -0600

    Fix bugs

--- bin/scripts/init-project/common.js
+++ bin/scripts/init-project/common.js
@@ -10,11 +10,13 @@ export default class Common extends BaseScript {
     await promises.cp(this.path+'tsconfig.json', process.cwd()+'/tsconfig.json');
     await promises.cp(this.path+'.env', process.cwd()+'/.env');
 
+    await this.copyFolderFromArchitectureFolder('infrastructure/errors/', 'entity');
     await this.copyFromArchitectureFolder('infrastructure/infrastructure.module.ts');
     await this.copyFromArchitectureFolder('infrastructure/setup.ts');
     await this.copyFromArchitectureFolder('app.module.ts');
     await this.copyFromArchitectureFolder('interface-adapters/controllers/controllers.module.ts');
     await this.copyFromArchitectureFolder('main.ts');
+    await this.execute(`mkdir -p ${process.cwd()}/test/factory`);
     await this.execute(`cp -rvf ${this.path}test ${process.cwd()}`);
   }
 }
